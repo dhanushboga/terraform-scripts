8a7a9900d4019dd199a94116955d825d468af703
dhanushboga <dhanushboga555@gmail.com>
2024-12-02 21:38:33 +0530

    creation of ec2-instance

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "linux" {
     ami = "ami-0453ec754f44f9a4a"
     instance_type = "t2.micro"
-    
+    key_name = "Linux-key"
+
+tags = {
+    Name = "Linux"
+} 
 }
-
-
-
 
