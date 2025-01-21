c5629099260945259a522ef6451f1eedb7261565
dhanushboga <dhanushboga555@gmail.com>
2025-01-21 22:14:37 +0530

    ec2 instance

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,38 @@
-resource "aws_instance" "linux" {
-    ami = "ami-0453ec754f44f9a4a"
-    instance_type = "t2.micro"
-    key_name = "Linux-key"
-    
+resource "aws_security_group" "allow-sshh"{
+    name   = "allow ssh"
+    description = "creating the allow ssh sg"
+
+    egress {
+       from_port        = 0
+       to_port          = 0
+       protocol         = "-1"
+       cidr_blocks      = ["0.0.0.0/0"]
+       ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_sshh"
+  }
+
+
+}
+
+
+resource "aws_instance" "terraform"{
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow-sshh.id]
+   
 tags = {
-    Name = "Linux"
+    Name = "Terraform"
 } 
 }
 
