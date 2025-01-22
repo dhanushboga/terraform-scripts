3bc3172781a4f1026e63a3623512640239627aa6
dhanushboga <dhanushboga555@gmail.com>
2025-01-22 21:28:28 +0530

    ec2-terraform

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,13 @@
-resource "aws_security_group" "allow-sshh"{
-    name   = "allow ssh"
-    description = "creating the allow ssh sg"
+resource "aws_security_group" "allow-sshh" {
+  name = "allow-sshh"
+  description = "allowing the ssh protocol"
 
-    egress {
-       from_port        = 0
-       to_port          = 0
-       protocol         = "-1"
-       cidr_blocks      = ["0.0.0.0/0"]
-       ipv6_cidr_blocks = ["::/0"]
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
@@ -18,20 +18,20 @@ resource "aws_security_group" "allow-sshh"{
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  tags = {
-    Name = "allow_sshh"
-  }
-
-
+tags = {
+  name = "Allow-sshh"
+}
 }
 
-resource "aws_instance" "terraform"{
+resource "aws_instance" "terraform" {
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow-sshh.id]
-   
+
+
 tags = {
-    Name = "Terraform"
-} 
+  name = "Terraform"
+}
+  
 }
 
