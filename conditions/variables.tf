bb9640d9d1370eb9d99b13abe125cd011b2e1dd4
dhanushboga <dhanushboga555@gmail.com>
2025-02-04 23:01:03 +0530

    conditions

---
+++ conditions/variables.tf
@@ -0,0 +1,66 @@
+variable "ami_id" {
+    type = string
+  default = "ami-09c813fb71547fc4f"
+}
+
+variable "aws_ins_type" {
+    type = string
+    default = "t3.micro"
+}
+
+variable "environment" {
+    default = "dev"
+  
+}
+
+
+variable "in_sg_from" {
+    type = number
+  default = 22
+}
+
+variable "in_sg_to" {
+    type = number
+    default = 22
+}
+
+variable "sg_in_protocol" {
+    type = string
+    default = "tcp"
+}
+
+variable "sg_in_cidr_block" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+  
+}
+
+variable "sg_in_ipv6" {
+    type = list(string)
+    default = ["::/0"]
+  
+}
+
+variable "tag_name_sg" {
+    default = {
+        name = "allow-sshh"
+        description = "test description"
+    }
+}
+
+variable "tag_name_ec2" {
+    default = {
+        Name = "MY EC2 INSTANCE"
+        purpose = "this is for practice"
+        Project = "expense"
+        component = "backend"
+        Environment = "expense-backend-dev"
+    }
+  
+}
+
+#command Line
+#terraform.tfvars
+#environment variables
+#default variables
+#prompt
