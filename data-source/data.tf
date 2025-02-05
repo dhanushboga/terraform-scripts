d8dc57d186eb168560cf0945097517178f9b66fc
dhanushboga <dhanushboga555@gmail.com>
2025-02-05 22:21:44 +0530

    data-source

---
+++ data-source/data.tf
@@ -0,0 +1,32 @@
+data "aws_ami" "my_ami_id" {
+  most_recent      = true
+  owners = ["973714476881"]
+
+  filter {
+    name   = "name"
+    values = ["RHEL-9-DevOps-Practice"]
+  }
+
+   filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+   name   = "virtualization-type"
+   values = ["hvm"]
+  }
+}
+
+data "aws_vpc" "default" {
+    default = true
+  
+}
+
+output "ami_info" {
+    value = data.aws_ami.my_ami_id.id
+}
+
+output "vpc_info" {
+    value = data.aws_vpc.default.id
+}
